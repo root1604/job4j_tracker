fb447338f9fdcd758d31efe6ea3769d003624073
Ilya Kasyanenko <villiakas@gmail.com>
2022-05-09 01:23:22 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("active: " + active);
+        System.out.println("status: " + status);
+        System.out.println("message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error error1 = new Error(true, 5, "Error 500");
+        error1.printInfo();
+        Error error2 = new Error(false, 3, "Error 400");
+        error2.printInfo();
+    }
+}
